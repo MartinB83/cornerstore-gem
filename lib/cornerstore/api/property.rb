2c8fa77e215ed17828797073d579d1bea7b883a4
Joachim Garth <jpgarth@googlemail.com>
2014-01-29 14:32:54 +0100

    Properties are now writable resources. Version bump to 0.1.2

--- lib/cornerstore/api/property.rb
+++ lib/cornerstore/api/property.rb
@@ -1,4 +1,6 @@
 class Cornerstore::Property < Cornerstore::Model::Base
+  include Cornerstore::Model::Writable
+
   attr_accessor :key,
                 :value
   
@@ -9,6 +11,20 @@ class Cornerstore::Property < Cornerstore::Model::Base
     }
   end
   
+  # TODO: Rewrite #url on base model to automatically detect parent/url depth.
+  def url
+    super(2)
+  end
+  
+  def initialize(attributes = {}, parent = nil)
+    @key = attributes.delete(:key)
+    @value = attributes.delete(:value)
+        
+    super
+  end
+  
   class Resource < Cornerstore::Resource::Base
+    include Cornerstore::Resource::Remote
+    include Cornerstore::Resource::Writable
   end
 end
