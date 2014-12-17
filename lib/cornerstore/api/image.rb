afdb861b70530a901fd584626604f0cb1437dd1c
Christian Weyer <caducitynights@gmail.com>
2014-12-17 11:48:46 +0100

    Make cloudinary URLs protocol-transparent

--- lib/cornerstore/api/image.rb
+++ lib/cornerstore/api/image.rb
@@ -12,7 +12,7 @@ class Cornerstore::Image < Cornerstore::Model::Base
 
   # small, small_square, medium, medium_square, large
   def url(w = 600, h = 600, mode = 'crop')
-    "http://res.cloudinary.com/hgzhd1stm/image/upload/c_#{mode},h_#{h},w_#{w}/#{self.key}"
+    "//res.cloudinary.com/hgzhd1stm/image/upload/c_#{mode},h_#{h},w_#{w}/#{self.key}"
   end
 
   class Resource < Cornerstore::Resource::Base
